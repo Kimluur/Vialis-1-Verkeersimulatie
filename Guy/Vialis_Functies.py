5a25e5ea44b8a480e9e497678ab8fc341ff573bb
AI-Gio <wreckitsnoofs@gmail.com>
2021-01-11 11:16:29 +0100

    Green, test velocity function: recognize activation

--- Guy/Vialis_Functies.py
+++ Guy/Vialis_Functies.py
@@ -1,2 +1,19 @@
+import csv
+
 def velocity():
-    pass
+    sensor = []
+    activation = False
+    with open('BOS210_Correct.csv') as f:
+        r = csv.reader(f, delimiter=';')
+        for i in r:
+            if i[38] == '|' and activation == False:
+                sensor.append(i[38])
+                activation = True
+            elif i[38] == '|' and activation == True:
+                sensor.append(i[38])
+            elif i[38] == '' and activation == True:
+                activation = False
+                break
+    return sensor
+
+print(velocity())
--- Guy/test.py
+++ Guy/test.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 from Guy import Vialis_Functies as vf
 
-
 class TestVelocity(TestCase):
     def test_velocity(self):
         self.assertIn("|", vf.velocity())

ec990e0b5233aca62d6612b1cb26ad71c9267cc4
Kwuffin <bestboykevin.bbg@gmail.com>
2021-01-14 17:11:13 +0100

    Leest routes uit de externe data, en maakt een .csv-bestand aan.

--- Kevin/Externe_data/readExternalData.py
+++ Kevin/Externe_data/readExternalData.py
@@ -11,6 +11,24 @@ def add_hour(datafr):
     datafr.timestamp = pd.DatetimeIndex(datafr.timestamp) + timedelta(hours=1)
 
 
+def get_routes(datafr):
+    routes = [[117, 193],
+              [282, 294],
+              [462, 476],
+              [1010, 1026],
+              [1101, 1115]]
+
+    route_1 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][117:193])
+    route_2 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][282:295])
+    route_3 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][462:477])
+    route_4 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][1010:1027])
+    route_5 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][1101:1116])
+
+    dataframes = route_1.append(route_2.append(route_3.append(route_4.append(route_5))))
+
+    return dataframes
+
+
 def main():
     df = pd.read_csv('1.csv', delimiter=";")
 
@@ -18,7 +36,8 @@ def main():
 
     add_hour(df)
 
-    print(df)
+    dfRoute = get_routes(df)
+    dfRoute.to_csv('routes.csv')
 
 
 if __name__ == '__main__':

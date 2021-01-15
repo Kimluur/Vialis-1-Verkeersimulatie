ca8ccb65c6bf1297f57622683c2d6df9c3acf639
AI-Gio <wreckitsnoofs@gmail.com>
2021-01-15 15:40:09 +0100

    red, test_wachtrij fails

--- Guy/test_wachtrij.py
+++ Guy/test_wachtrij.py
@@ -1,11 +1,22 @@
 from unittest import TestCase
 from Guy.wachtrij import tel
 from Guy.Vialis_Functies import csv2pd
+from Guy.wachtrij import wachtrij_achteraf
 
 class TestWachtrij(TestCase):
+
     def test_wachtrij(self):
         """
         Check for wrong input, check how many times a sensor is activated (not how many 10th of a seconds its activated)
         """
+
+        # self.assertEqual('2', tel(df['011']))
+
+    def test_wachtrij_achteraf(self):
+        """
+        Checks for wrong input, checks if the cars are caculated correctly and if the data is correctly added to the dictionary
+        """
         df = csv2pd('test.csv')
-        self.assertEqual('2', tel(df['011']))
+        rijbanen = [['01','011']]
+        eind = wachtrij_achteraf(rijbanen, df)
+        self.assertEqual(1, list(eind[rijbanen[0][2]].values())[0])

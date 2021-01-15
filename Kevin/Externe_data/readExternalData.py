37cbcc708d31fef80264aa5fb86c8c59ca2c3eab
Kwuffin <bestboykevin.bbg@gmail.com>
2021-01-15 16:21:01 +0100

    Missende tijden opgevuld, intervallen van 100ms

--- Kevin/Externe_data/readExternalData.py
+++ Kevin/Externe_data/readExternalData.py
@@ -11,33 +11,26 @@ def add_hour(datafr):
     datafr.timestamp = pd.DatetimeIndex(datafr.timestamp) + timedelta(hours=1)
 
 
-def get_routes(datafr):
-    routes = [[117, 193],
-              [282, 294],
-              [462, 476],
-              [1010, 1026],
-              [1101, 1115]]
-
-    route_1 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][117:193])
-    route_2 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][282:295])
-    route_3 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][462:477])
-    route_4 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][1010:1027])
-    route_5 = pd.DataFrame(datafr[['timestamp', 'latitude', 'longitude', 'speed']][1101:1116])
-
-    dataframes = route_1.append(route_2.append(route_3.append(route_4.append(route_5))))
-
-    return dataframes
-
-
 def main():
+    # Lees CSV
     df = pd.read_csv('1.csv', delimiter=";")
 
+    # Converteer 'timestamp' om naar datetime
     df.timestamp = df.timestamp.str.slice(stop=19)
 
+    # Voeg één uur toe aan tijd, als correctie
     add_hour(df)
 
-    dfRoute = get_routes(df)
-    dfRoute.to_csv('routes.csv')
+    # Vul tijden in met milliseconden
+    df = df.set_index('timestamp').resample("100ms").first().reset_index().reindex(columns=df.columns)
+    cols = df.columns.difference(
+        ['latitude', 'longitude'])
+    df[cols] = df[cols].ffill()
+
+    # Zet tijd in juiste format
+    df['timestamp'] = df["timestamp"].dt.strftime("%m-%d-%Y %H:%M:%S.%f")
+
+    print(df)
 
 
 if __name__ == '__main__':

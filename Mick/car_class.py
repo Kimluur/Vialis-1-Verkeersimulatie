5228a428d6109cbc429474af73b09b99295a8e49
Jellestiesri <you@example.comjellestiesri@gmail.com>
2021-01-19 12:06:35 +0100

    Refactor - Car_class Pep8
    Car_class opgeschoond en Pep8 correct gemaakt

--- Mick/car_class.py
+++ Mick/car_class.py
@@ -1,25 +1,21 @@
 from coordFunctions import *
 import pygame
 
-class NewCar(pygame.sprite.Sprite):
-    def __init__(self, position,  df, color):
+
+class Car(pygame.sprite.Sprite):
+    def __init__(self,  df, color):
         super().__init__()
-        self.position = position
         self.image = pygame.Surface((12, 12))  # De auto (12x12 pixel rechthoek)
         self.image.fill(pygame.Color(color))
-        self.rect = self.image.get_rect(center=position)
+        self.position = [-200, -200]  # Positie buiten het scherm
+        self.rect = self.image.get_rect(center=self.position)  # Tekent het vierkantje
         self.df = df
 
     def update(self, timestamp):
+        """De update functie wordt elke frame gerund. Hierin word de nieuwe positie van de auto berekend"""
+        df_cor = self.df.loc[self.df['timestamp'] == timestamp][['latitude', 'longitude']]
 
-        df_cor = self.df.loc[self.df['timestamp'] == timestamp][['latitude','longitude']]
-        # self.position[0] += 10
-
-        if len(df_cor) == 1:
+        if len(df_cor) == 1:  # Doet geen update als er geen positie is voor de auto op deze tijd
             df_cor = df_cor.reset_index()
             self.position = latlngToScreenXY(df_cor['latitude'][0], df_cor['longitude'][0])
-            # print(timestamp)
-            # print(self.position)
             self.rect.center = self.position
-        else:
-            pass

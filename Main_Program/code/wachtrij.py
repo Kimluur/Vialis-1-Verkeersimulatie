5d8b0d21236d7978cb4ea0a2343407f0c0a0b8e8
AI-Gio <60139940+AI-Gio@users.noreply.github.com>
2021-01-22 12:52:36 +0100

    Update wachtrij.py
    Wachtrij.csv is now written in correct folder

--- Main_Program/code/wachtrij.py
+++ Main_Program/code/wachtrij.py
@@ -184,8 +184,9 @@ def wachtrij_csv(rijbanen, df):
     df_w = df_w.fillna(0)
 
     df_w.insert(0, "time", df_w.index)
-    df_w.to_csv(r'C:\Users\gsvpk\PycharmProjects\Vialis-1-Verkeersimulatie(new)'+'Wachtrij.csv',index=False)
-
+    df_w.to_csv(r'C:\Users\gsvpk\PycharmProjects\Vialis-1-Verkeersimulatie'+'Wachtrij.csv',index=False)
+    # if Wachtrij.csv isn't written in Vialis-1-Verkeersimulatie, the file should be placed in that folder
+    
 wachtrij_csv(lane_group(df), df)
 
 
